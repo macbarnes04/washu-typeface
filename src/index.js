253b4289968c190bd6ac5b708f5203c9d2067c7d
“Mac <b.mac@wustl.edugit congit config --global user.name Mac>
2025-02-12 09:56:38 -0600

    pages

--- src/index.js
+++ src/index.js
@@ -4,11 +4,10 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/washu-typeface">
     <App />
   </BrowserRouter>
 );
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
